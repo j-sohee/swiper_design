6bb11277f1f3311e7809ca2f30dc553ac318347f
j-sohee <sohee.jin@gmail.com>
2021-11-30 18:37:48 +0900

    버튼 클릭시 배경 이미지 활성화 기능 추가

--- js/main.js
+++ js/main.js
@@ -31,3 +31,32 @@ const swiper = new Swiper("#wrap", {
     }
     */
 });
+
+//Dom Caching
+const bgs = document.querySelectorAll(".bg li");
+const prev = document.querySelector(".swiper-button-prev");
+const next = document.querySelector(".swiper-button-next");
+const navi = document.querySelectorAll(".swiper-pagination span")
+
+next.addEventListener("click", activation);
+prev.addEventListener("click", activation);
+window.addEventListener("mousewheel", activation);
+swiper.on("slideChangeTransitionEnd", activation);
+
+for(let el of navi){
+    el.addEventListener("click", e=>{
+        const isOn = e.currentTarget.classList.contatins("swiper-pagination-bullet-active");
+        if(isOn) return;
+        swiper.on("slideChangeTransitionEnd", activation);
+    })
+}
+
+function activation(){
+    let item = document.querySelector(".swiper-slide-active");
+    let i = item.getAttribute("data-swiper-slide-index");
+    
+    for(el of bgs){
+        el.classList.remove("on");
+    }
+    bgs[i].classList.add("on");
+}
