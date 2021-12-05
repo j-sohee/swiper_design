37e2dbad1007088eff624fba3c5a5247eea0cb7b
j-sohee <sohee.jin@gmail.com>
2021-12-05 20:44:05 +0900

    about layout 적용

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-const menu = ["Company", "About", "Information", "News"];
+const menu = ["ABOUT", "PORTFOLIO", "CONTACT", "News"];
 
 const swiper = new Swiper("#wrap", {
     loop : true,
     slidesPerView : "auto",
     centeredSlides:true,
-    spaceBetween:50,
+    spaceBetween:150,
     mousewheel : true,
 
     navigation:{
