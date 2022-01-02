70f8e3a8972e582438bdf8d7372551d3d3c6223d
j-sohee <sohee.jin@gmail.com>
2022-01-02 20:32:43 +0900

    swiper 레이아웃 변경중

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const menu = ["ABOUT", "PORTFOLIO", "CONTACT"];
+const menu = ["ABOUT", "WebSite(Jquery)","WebSite(Javascript)","Grid(Isotope)", "CONTACT"];
 
 const swiper = new Swiper("#wrap", {
     loop : false,
